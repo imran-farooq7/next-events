f0360eebdf2a582c0b857a9597bc29ac7d96506c
Imran Farooq <imran_farooq7@hotmail.com>
2024-08-14 20:49:51 +0500

    params added to dynamic routes

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,8 +1,13 @@
-const EventDetails = () => {
+interface Props {
+	params: {
+		slug: string;
+	};
+}
+const EventDetails = ({ params }: Props) => {
 	return (
 		<main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 			<h1 className="mb-6 mt-4 text-xl font-bold lg:text-2xl text-black">
-				Events in Delhi
+				Events in <span className="capitalize">{params.slug}</span>
 			</h1>
 		</main>
 	);
