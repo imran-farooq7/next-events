b1d3c2f1939a90550d51efde9853b3037be1a7d5
Imran Farooq <imran_farooq7@hotmail.com>
2024-08-14 20:32:24 +0500

    events page styling completed

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,13 +1,8 @@
-interface Props {
-	params: {
-		slug: string;
-	};
-}
-const EventDetails = ({ params }: Props) => {
+const EventDetails = () => {
 	return (
 		<main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 			<h1 className="mb-6 mt-4 text-xl font-bold lg:text-2xl text-black">
-				Events in {params.slug}
+				Events in Delhi
 			</h1>
 		</main>
 	);
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,11 @@
 const EventsPage = () => {
-	return <div></div>;
+	return (
+		<main className="mx-auto py-12 max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+			<h1 className="mb-6 mt-4 text-xl font-bold lg:text-2xl text-black">
+				All Events
+			</h1>
+		</main>
+	);
 };
 
 export default EventsPage;
